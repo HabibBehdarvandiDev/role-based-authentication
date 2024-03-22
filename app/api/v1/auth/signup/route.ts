b1dd5f38fee886d608e0798d8f378f60f6a519e0
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-03-22 19:11:40 +0330

    create authToken Middleware

--- app/api/v1/auth/signup/route.ts
+++ app/api/v1/auth/signup/route.ts
@@ -3,8 +3,39 @@ import prisma from "@/db/prisma";
 import { SignUpRequestSchema } from "./schema";
 import { UserRole } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { authenticate } from "../../authMiddleware";
 
 export async function POST(req: NextRequest) {
+  /* const { headers } = req;
+
+  if (!headers.has("authToken")) {
+    return NextResponse.json({
+      error: "Unauthorized: Missing AuthToken",
+    });
+  }
+
+  const authToken = headers.get("authToken");
+
+  const tokenVerification = await jwt.verify(
+    authToken!,
+    process.env.JWT_SECRET!
+  );
+
+  if (!tokenVerification) {
+    return NextResponse.json({
+      error: "Unauthorized: Invalid AuthToken",
+    });
+  }
+ */
+
+  const isAuthenticated = await authenticate(req);
+  if (!isAuthenticated) {
+    return NextResponse.json({
+      error: "Unauthorized: Invalid or Missing AuthToken",
+    });
+  }
+
   let body;
   try {
     body = await req.json();
